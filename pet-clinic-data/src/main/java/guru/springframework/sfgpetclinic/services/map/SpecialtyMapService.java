64e0ed0be5944037ab698d892c81f07df81b467c
Heijstekske <heijstekker@gmail.com>
2021-12-29 14:42:35 +0100

    Update Profiles to use Spring Data JPA Services closes #16

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -10,29 +11,30 @@ import java.util.Set;
  * @author NH 2021-09-16
  */
 @Service
+@Profile({"default", "mapService"})
 public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
-    @Override
-    public Set<Specialty> findAll() {
-        return super.findAll();
-    }
+   @Override
+   public Set<Specialty> findAll() {
+      return super.findAll();
+   }
 
-    @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
-    }
+   @Override
+   public void deleteById(Long id) {
+      super.deleteById(id);
+   }
 
-    @Override
-    public void delete(Specialty object) {
-        super.delete(object);
-    }
+   @Override
+   public void delete(Specialty object) {
+      super.delete(object);
+   }
 
-    @Override
-    public Specialty save(Specialty object) {
-        return super.save(object);
-    }
+   @Override
+   public Specialty save(Specialty object) {
+      return super.save(object);
+   }
 
-    @Override
-    public Specialty findById(Long id) {
-        return super.findById(id);
-    }
+   @Override
+   public Specialty findById(Long id) {
+      return super.findById(id);
+   }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -12,46 +13,46 @@ import java.util.Set;
  * @author NH 2021-08-19
  */
 @Service
+@Profile({"default", "mapService"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
-    private final SpecialtyService specialtyService;
+   private final SpecialtyService specialtyService;
 
-    public VetMapService(SpecialtyService specialtyService) {
-        this.specialtyService = specialtyService;
-    }
+   public VetMapService(SpecialtyService specialtyService) {
+      this.specialtyService = specialtyService;
+   }
 
-    @Override
-    public Set<Vet> findAll() {
-        return super.findAll();
-    }
+   @Override
+   public Set<Vet> findAll() {
+      return super.findAll();
+   }
 
-    @Override
-    public Vet findById(Long id) {
-        return super.findById(id);
-    }
+   @Override
+   public Vet findById(Long id) {
+      return super.findById(id);
+   }
 
-    @Override
-    public Vet save(Vet object) {
+   @Override
+   public Vet save(Vet object) {
 
-    if(object.getVetSpecialties().size() > 0){
-        object.getVetSpecialties().forEach(specialty -> {
-            if(specialty.getId() == null){
-                Specialty savedSpecialty = specialtyService.save(specialty);
-                specialty.setId(savedSpecialty.getId());
+      if (object.getVetSpecialties().size() > 0) {
+         object.getVetSpecialties().forEach(specialty -> {
+            if (specialty.getId() == null) {
+               Specialty savedSpecialty = specialtyService.save(specialty);
+               specialty.setId(savedSpecialty.getId());
             }
-        });
-    }
+         });
+      }
+      return super.save(object);
+   }
 
-        return super.save(object);
-    }
+   @Override
+   public void deleteById(Long id) {
+      super.deleteById(id);
+   }
 
-    @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
-    }
-
-    @Override
-    public void delete(Vet object) {
-        super.delete(object);
-    }
+   @Override
+   public void delete(Vet object) {
+      super.delete(object);
+   }
 }
